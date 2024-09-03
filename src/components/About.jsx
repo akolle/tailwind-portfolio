8fd06e719c3991893da74bf24f2fa1b562e5ac9e
akolle <andykoller55@yahoo.com>
2024-09-03 19:56:39 -0400

    about section

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import aboutSvg from '../assets/about.svg'
+import SectionTitle from './SectionTitle'
+
+const About = () => {
+  return (
+    <section className="bg-white py-20" id="about">
+      <div className="align-element grid md:grid-cols-2 items-center gap-16">
+        <img src={aboutSvg} alt="about image" className="w-full h-64" />
+        <article>
+          <SectionTitle text="code and coffee" />
+          <p className="text-slate-600 mt-8 leading-loose">
+            Started with Excel. Graduated to Tableau. Now building with React!
+          </p>
+        </article>
+      </div>
+    </section>
+  )
+}
+export default About
